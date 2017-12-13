9434d026f7e5b38728f8490c8b98ec45818058cf
Dante Solorio <ddasogaa@gmail.com>
2017-12-13 12:48:49 -0600

    Animate circular bar with download data

--- CircularProgressExample/ViewController.swift
+++ CircularProgressExample/ViewController.swift
@@ -8,22 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
+    
+    // MARK: - UI elements
+    
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        return label
+    }()
+    
+    // MARK: - Properties
 
     let shapeLayer = CAShapeLayer()
+    
+    // MARK: - LifeCicle functinos
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let center = view.center
+        // Add percentage label
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
         
         // Create my track layer
         let trackLayer = CAShapeLayer()
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         trackLayer.path = circularPath.cgPath
         trackLayer.strokeColor = UIColor.lightGray.cgColor
         trackLayer.lineWidth = 10
         trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.position = view.center
         view.layer.addSublayer(trackLayer)
         
         // Shape layer animated
@@ -32,6 +50,9 @@ class ViewController: UIViewController {
         shapeLayer.lineWidth = 10
         shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.lineCap = kCALineCapRound
+        shapeLayer.position = view.center
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+        
         shapeLayer.strokeEnd = 0
         
         view.layer.addSublayer(shapeLayer)
@@ -39,14 +60,47 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     
-    // MARK: - Handles functions
-    @objc private func handleTap(){
+    // MARK: - Private functions
+    let urlString = "https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c"
+    
+    private func beginDownloadingFile(){
+        print("beginDownloadingFile")
+        shapeLayer.strokeEnd = 0
+        let configuration = URLSessionConfiguration.default
+        let operationQueue = OperationQueue()
+        let urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: operationQueue)
+        
+        guard let url = URL(string: urlString) else { return }
+        let downloadTask = urlSession.downloadTask(with: url)
+        downloadTask.resume()
+    }
+    
+    fileprivate func animateCircle() {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         basicAnimation.toValue = 1
         basicAnimation.duration = 1.5
         basicAnimation.fillMode = kCAFillModeForwards
         basicAnimation.isRemovedOnCompletion = false
         shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+    }
+    
+    // MARK: - Handles functions
+    @objc private func handleTap(){
+        beginDownloadingFile()
+    }
+    
+    // MARK: - URL session delegate functions
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("Finish downloading")
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+        DispatchQueue.main.async {
+            self.percentageLabel.text = "\(Int(percentage * 100))%"            
+            self.shapeLayer.strokeEnd = percentage
+        }
+        print(percentage)
     }
 }
 
